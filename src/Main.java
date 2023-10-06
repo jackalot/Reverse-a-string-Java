c270cc9f5afb10d318202b704f512abc3fbd4e62
jackalot <68657425+jackalot@users.noreply.github.com>
2023-10-06 10:01:31 -0400

    created for loop to Main
    added a newString variable and for loop that decrements each iteration.

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ import javax.swing.JOptionPane;
 public class Main {
 	public static void main(String[] args) {
 		String input = JOptionPane.showInputDialog("Enter a word to reverse, I swear its magical!");
+		String newString = "";
+		for(int i = input.length() - 1; i > -1; i--)
+		{
+			
+		}
+		
 	}
 }
