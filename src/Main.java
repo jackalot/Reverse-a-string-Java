3b203929b29d0bbfe60b95fe2990415dd6e7b33c
jackalot <68657425+jackalot@users.noreply.github.com>
2023-10-06 10:03:20 -0400

    created output post forloop
    we now ad a character each iteration, and add the message to output after we loop through the input.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import javax.swing.JOptionPane;
+
 public class Main {
 	public static void main(String[] args) {
 		String input = JOptionPane.showInputDialog("Enter a word to reverse, I swear its magical!");
 		String newString = "";
-		for(int i = input.length() - 1; i > -1; i--)
-		{
-			
+		for (int i = input.length() - 1; i > -1; i--) {
+			newString += input.charAt(i);
 		}
-		
+		JOptionPane.showMessageDialog(null, "Your magical new message is: \n" + newString);
 	}
 }
