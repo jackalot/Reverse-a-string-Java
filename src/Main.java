e4f986953102049362ae03af94ff973256895d72
jackalot <68657425+jackalot@users.noreply.github.com>
2023-10-06 09:59:46 -0400

    created an input prompt
    added an input prompt for main, asks which word you would like to reverse.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import javax.swing.JOptionPane;
 public class Main {
 	public static void main(String[] args) {
+		String input = JOptionPane.showInputDialog("Enter a word to reverse, I swear its magical!");
 	}
 }
